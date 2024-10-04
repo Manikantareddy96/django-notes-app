c63c5ca07d3720f885941ee260a78b81028b789b
Manikantareddy96 <167765029+Manikantareddy96@users.noreply.github.com>
2024-10-04 18:34:38 +0530

    Update data.js

--- mynotes/src/assets/data.js
+++ mynotes/src/assets/data.js
@@ -1,7 +1,7 @@
 let notes = [
     {
         "id": 1,
-        "body": "Todays Agenda\n\n- Walk Dog\n- Feed fish\n- Play basketball\n- Eat a salad",
+        "body": "Todays plan\n\n- Walk Dog\n- Feed fish\n- Play basketball\n- Eat a salad",
         "updated": "2021-07-14T13:49:02.078653Z",
     },
     {
